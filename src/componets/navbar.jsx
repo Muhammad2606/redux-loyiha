1c6d0cdbdd7122ca4ea1db5bee9cce84615cfc77
Your Name <you@example.com>
2023-12-25 20:12:32 +0500

    responsive

--- src/componets/navbar.jsx
+++ src/componets/navbar.jsx
@@ -1,51 +1,51 @@
 import { Link, useNavigate } from "react-router-dom"
-import {logo} from '../constants'
+import { logo } from '../constants'
 import { useDispatch, useSelector } from "react-redux"
 import { removeItem } from "../helpers/persistens-storege"
 import { logoutUser } from "../slice/auth"
 const Navbar = () => {
-  const {loggedIn,user} = useSelector(state => state.auth)
+  const { loggedIn, user } = useSelector(state => state.auth)
 
   const navigate = useNavigate()
-const dispatch = useDispatch()
-  const Longout =() =>{
+  const dispatch = useDispatch()
+  const Longout = () => {
     removeItem('token')
     dispatch(logoutUser())
     navigate('/login')
   }
 
   return (
-    <div className="d-flex  navbar align-items-center pb-3 mb-4 border-bottom container pt-3">
-        
-        <Link to={'/'}  >
-            <img src={logo} alt="" width={150}/>
-        </Link>
+    <div className="d-flex w-100  justify-content-xxl-between justify-content-xl-between justify-content-lg-between justify-content-sm-center justify-content-md-between align-items-center pb-3 mb-4 border-bottom container pt-3 flex-sm-column  flex-column flex-md-row ">
+
+      <Link to={'/'}  >
+        <img src={logo} alt="" width={150} />
+      </Link>
 
 
       {loggedIn ? (
-        <>
-      <p className="fs-4 text-capitalize fw-bold">{user.username}</p>
-        <div>
-        <Link className="me-3 py-2 link-body-emphasis text-success text-decoration-none" to={'/create-article'}>Create article</Link>
+        <div className="d-flex gap-3 text-center justify-center mt-3">
+          <p className="fs-4 text-capitalize fw-bold">{user.username}</p>
+          <div>
+            <Link className="me-3 py-2 link-body-emphasis text-success text-decoration-none" to={'/create-article'}>Create article</Link>
 
-<button className="btn btn-danger" onClick={Longout}>Longout </button>
+            <button className="btn btn-danger" onClick={Longout}>Longout </button>
+          </div>
+
         </div>
 
-        </>
-
-)
+      )
 
         : (
 
-      <>
-      <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-        <Link className="me-3 py-2 link-body-emphasis text-decoration-none" to={'/login'}>Login</Link>
-        <Link className="me-3 py-2 link-body-emphasis text-decoration-none" to={'/register'}>Register</Link>
-   
-      </nav>
-      </>
+          <>
+            <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+              <Link className="me-3 py-2 link-body-emphasis text-decoration-none" to={'/login'}>Login</Link>
+              <Link className="me-3 py-2 link-body-emphasis text-decoration-none" to={'/register'}>Register</Link>
+
+            </nav>
+          </>
         )
-}
+      }
 
 
     </div>
@@ -53,3 +53,6 @@ const dispatch = useDispatch()
 }
 
 export default Navbar
+
+
+
